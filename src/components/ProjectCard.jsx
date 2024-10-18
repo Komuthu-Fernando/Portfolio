cf01ea6e0ed5198d2b47689e4b5d68338e65e503
Komuthu <komuthuapsara@gmail.com>
2024-10-18 19:15:46 +0530

    project card / home adj

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,22 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/ProjectCard.css'; 
 
 const ProjectCard = ({ logo, title, year, image }) => {
   const [hovered, setHovered] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <div className='project-card-container'>
-    <div
-      className={`project-card ${hovered ? 'hovered' : ''}`}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
-      <div className="logo-container">
-        <img src={hovered ? logo.color : logo.ash} alt={title} className="logo" />
+      <div
+        className={`project-card ${hovered ? 'hovered' : ''}`}
+        onMouseEnter={() => !isMobile && setHovered(true)}
+        onMouseLeave={() => !isMobile && setHovered(false)}
+      >
+        <div className="logo-container">
+          <img src={isMobile || hovered ? logo.color : logo.ash} alt={title} className="logo" />
+        </div>
+        {hovered && <img src={image} alt={title} className="background-image" />}
       </div>
-      {hovered && <img src={image} alt={title} className="background-image" />}
-    </div>
-    <p className='title'>{title} <span className='year'>{year}</span></p>
+      <p className='title'>{title} <span className='year'>{year}</span></p>
     </div>
   );
 };
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,88 +4,94 @@ import '../css/Home.css';
 import Projects from '../screens/Projects';
 import About from './About';
 import Contact from './Contact';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLocationArrow } from '@fortawesome/free-solid-svg-icons'
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationArrow } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
-  const { ref: heroRef, inView: heroInView } = useInView({ triggerOnce: true });
-  const { ref: projectsRef, inView: projectsInView } = useInView({ triggerOnce: true });
-  const { ref: aboutRef, inView: aboutInView } = useInView({ triggerOnce: true });
-  const { ref: contactRef, inView: contactInView } = useInView({ triggerOnce: true });
+	const { ref: heroRef, inView: heroInView } = useInView({ triggerOnce: true });
+	const { ref: projectsRef, inView: projectsInView } = useInView({
+		triggerOnce: true,
+	});
+	const { ref: aboutRef, inView: aboutInView } = useInView({
+		triggerOnce: true,
+	});
+	const { ref: contactRef, inView: contactInView } = useInView({
+		triggerOnce: true,
+	});
 
-  return (
-    <div>
-      
-      <div className="home-container" id="/">
-        <motion.div
-          className="hero-section"
-          ref={heroRef}
-          initial={{ opacity: 0, y: 50 }}
-          animate={heroInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-          transition={{ duration: 0.8 }}
-        >
-          <div>
-            <span className="about-title">Hello, I'm Komuthu 👋</span>
-          </div>
-          <motion.h1
-            className="title"
-            initial={{ opacity: 0, y: 50 }}
-            animate={heroInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-            transition={{ delay: 0.2, duration: 0.8 }}
-          >
-            Full Stack Developer <span className="title-sub">& Designer crafting innovative solutions</span>
-          </motion.h1>
-          <a href="mailto:komuthuapsara@gmail.com">
-          <motion.button
-            className="email-button"
-            initial={{ opacity: 0, y: 50 }}
-            animate={heroInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-            transition={{ delay: 0.6, duration: 0.8 }}
-            whileHover="hover"
-          >
-            Email Me{" "}
-              <FontAwesomeIcon icon={faLocationArrow} />
-          </motion.button>
-          </a>
-        </motion.div>
-      </div>
+	return (
+		<div>
+			<div className="home-container" id="/">
+				<motion.div
+					className="hero-section"
+					ref={heroRef}
+					initial={{ opacity: 0, y: 50 }}
+					animate={heroInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+					transition={{ duration: 0.8 }}
+				>
+					<div>
+						<span className="about-title">Hello, I'm Komuthu 👋</span>
+					</div>
+					<motion.h1
+						className="title"
+						initial={{ opacity: 0, y: 50 }}
+						animate={heroInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+						transition={{ delay: 0.2, duration: 0.8 }}
+					>
+						Full Stack Developer{' '}
+						<span className="title-sub">
+							& Designer crafting innovative solutions
+						</span>
+					</motion.h1>
 
-      <div id="work"></div>
-      <div className="project-container" ref={projectsRef}>
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          animate={projectsInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-          transition={{ duration: 0.8 }}
-        >
-          <Projects />
-        </motion.div>
-      </div>
+					<motion.button
+						className="email-button"
+						initial={{ opacity: 0, y: 50 }}
+						animate={heroInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+						transition={{ delay: 0.6, duration: 0.8 }}
+						whileHover="hover"
+					>
+						<a href="mailto:komuthuapsara@gmail.com" className="a-tag">
+							Email Me <FontAwesomeIcon icon={faLocationArrow} />
+						</a>
+					</motion.button>
+				</motion.div>
+			</div>
 
-      
-      <div className="aboutus-container" id="about" ref={aboutRef}>
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          animate={aboutInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-          transition={{ duration: 0.8 }}
-        >
-          <About />
-        </motion.div>
-      </div>
+			<div id="work"></div>
+			<div className="project-container" ref={projectsRef}>
+				<motion.div
+					initial={{ opacity: 0, y: 50 }}
+					animate={
+						projectsInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }
+					}
+					transition={{ duration: 0.8 }}
+				>
+					<Projects />
+				</motion.div>
+			</div>
 
-      
-      <div id="contact" ref={contactRef}>
-        <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          animate={contactInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
-          transition={{ duration: 0.8 }}
-        >
-          <Contact />
-        </motion.div>
-      </div>
-    </div>
-  );
+			<div className="aboutus-container" id="about" ref={aboutRef}>
+				<motion.div
+					initial={{ opacity: 0, y: 50 }}
+					animate={aboutInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+					transition={{ duration: 0.8 }}
+				>
+					<About />
+				</motion.div>
+			</div>
+
+			<div id="contact" ref={contactRef}>
+				<motion.div
+					initial={{ opacity: 0, y: 50 }}
+					animate={contactInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+					transition={{ duration: 0.8 }}
+				>
+					<Contact />
+				</motion.div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
